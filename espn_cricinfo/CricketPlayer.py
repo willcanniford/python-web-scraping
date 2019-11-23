37c9e6862f14ab35e81d942f0f189bec9d77b912
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-23 16:01:03 +0000

    Alter innings function
    Change basic innings to raw_innings, and have clean version for innings to return clean pd.DataFrame

--- espn_cricinfo/CricketPlayer.py
+++ espn_cricinfo/CricketPlayer.py
@@ -2,6 +2,7 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import numpy as np
 
 # Define the class for a player
 class CricketPlayer:
@@ -12,7 +13,7 @@ class CricketPlayer:
     def view_raw_html(self):
         return(self.soup)
     
-    def innings(self):
+    def raw_innings(self):
         for caption in self.soup.find_all('caption'):
             if caption.get_text() == 'Innings by innings list':
                 main_table = caption.find_parent('table', {'class': 'engineTable'})
@@ -24,3 +25,12 @@ class CricketPlayer:
             rows.append([stat.get_text() for stat in innings.find_all('td')])
             
         return(pd.DataFrame(rows, columns=columns))
+    
+    def innings(self):
+        raw_innings = self.raw_innings()
+        raw_innings['Opposition'] = raw_innings['Opposition'].str.replace('v ', '')
+        raw_innings.replace('-', np.nan, inplace=True)
+        raw_innings.columns = raw_innings.columns.str.lower().str.replace(' ', '_')
+        raw_innings['is_out'] = raw_innings.score.astype('str').apply(lambda x: np.nan if x == 'nan' else False if '*' in x else True)
+        raw_innings['did_bowl'] = raw_innings.overs.astype('str').apply(lambda x: False if x in ['nan', 'DNB'] else True)
+        return(raw_innings[['inns', 'score', 'is_out', 'overs', 'conc', 'wkts', 'did_bowl', 'ct', 'st', 'opposition', 'ground', 'start_date']])
