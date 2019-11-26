b5a829ddd296f23b2226d6ceaa13b64e0be48a5e
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-26 19:59:44 +0000

    Move cricket web scraping to new repository

--- espn_cricinfo/command_line_testing.py
+++
@@ -1,5 +0,0 @@
-from CricketPlayer import CricketPlayer
-
-joe_root = 'http://stats.espncricinfo.com/ci/engine/player/303669.html?class=1;template=results;type=allround;view=innings'
-
-print(CricketPlayer(joe_root).rolling_average_innings(5))
